b7cf273da308087d22f3a94030ee794d07948afd
lanou <798729330@qq.com>
2015-09-09 15:45:40 +0800

    test

---
+++ aaa/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  aaa
+//
+//  Created by lanou on 15/9/9.
+//  Copyright (c) 2015年 Ezio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+- (void)say;
+- (void)sayHi;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self say];
+    [self sayHi];
+}
+
+- (void)say
+{
+    NSLog(@"sss");
+}
+
+- (void)sayHi
+{
+    NSLog(@"hi");
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
